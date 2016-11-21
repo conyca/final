bf366006bb8bc36fd4659cb08bf4a945103719ff
태통령 <@TTL>
2016-11-21 17:03:53 +0900

    테스트

---
+++ final/src/main/java/com/pknu/pro/main/service/MainService.java
@@ -0,0 +1,5 @@
+package com.pknu.pro.main.service;
+
+public interface MainService {
+
+}
---
+++ final/src/main/java/com/pknu/pro/main/service/MainServiceImpl.java
@@ -0,0 +1,5 @@
+package com.pknu.pro.main.service;
+
+public class MainServiceImpl implements MainService {
+
+}
