dc688912023f8e0bedd3c1e876ca661349b74bc7
PKNU <PKNU@PKNU-PC>
2016-11-25 16:27:12 +0900

    dz

--- final/src/main/resources/etc/final.sql
+++ final/src/main/resources/etc/final.sql
@@ -1,13 +1,13 @@
 DROP TABLE member;
 CREATE TABLE member (
   memberno NUMBER PRIMARY KEY,
-  id VARCHAR2(15) NOT NULL,
+  id VARCHAR2(20) NOT NULL UNIQUE,
   pass VARCHAR2(20) NOT NULL,
   name VARCHAR2(10) NOT NULL,
   sex NUMBER(1) NOT NULL,
   birthday VARCHAR2(10) NOT NULL,
-  email VARCHAR2(50) NOT NULL,
-  mobno VARCHAR2(15) NOT NULL,
+  email VARCHAR2(50) NOT NULL UNIQUE,
+  mobno VARCHAR2(15) NOT NULL UNIQUE,
   status VARCHAR2(5) NOT NULL,
   category NUMBER NOT NULL,
   regdate VARCHAR2(10) NOT NULL,
@@ -15,3 +15,8 @@ CREATE TABLE member (
   datano NUMBER
   -- datano foreign key 
 );
+
+CREATE SEQUENCE mem_seq
+  START WITH 1
+  INCREMENT BY 1;
+
